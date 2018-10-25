022c7b57874796809ae997ddc5e341f0f4aa195a
valkyr1e <nkpoidjp+tera@gmail.com>
2018-10-26 04:39:22 +0900

    fix for p75

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = function SkillResets(mod) {
     mod.hook('S_CREST_MESSAGE', 2, ({type, skill}) => {
         if (type === 6) {
             mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
-                message: `<img src="img://skill__0__${mod.game.me.templateId}__${skill}" width="48" height="48" vspace="-20"/><font size="24" color="${mod.settings.reset_font_color}">&nbsp;Reset</font>`,
-                type: mod.settings.flashing_notification ? 70 : 2,
+                message: `<img src="img://skill__0__${mod.game.me.templateId}__${skill}" width="36" height="36" vspace="-20"/><font size="24" color="${mod.settings.reset_font_color}">&nbsp;Reset</font>`,
+                type: 22,
                 chat: false,
                 channel: 0,
             })
